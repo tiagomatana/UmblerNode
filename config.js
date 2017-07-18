0f6b1c3e855cf894e7c7d8dab372c3dccaffa6d4
Tiago Matana <tiago.matana@gmail.com>
2017-07-18 11:09:07 -0300

    publish website app

---
+++ config.js
@@ -0,0 +1,21 @@
+>th = require('path'),
+    config;
+
+config = {
+    production: {
+        url: 'http://localhost:3000',
+        mail: {},
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+
+        server: {
+            port: '3000',
+            host: '0.0.0.0'
+        }
+    }
+}
